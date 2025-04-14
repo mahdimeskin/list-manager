023620d62b9783c720fa45a501cb6ffc1e0190b3
Mahdi <meskinmahdi146@gmail.com>
2025-04-14 13:04:28 +0430

    connect to database

---
+++ models/connect.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	DB = db
+	fmt.Println("Connected to MySQL successfully")
+}
