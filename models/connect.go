002c19036a0c52d2e03d3c772d491b3ab8210c37
Mahdi <meskinmahdi146@gmail.com>
2025-04-20 15:30:05 +0430

    fix(db): fixed database connection problem

--- models/connect.go
+++ models/connect.go
@@ -11,11 +11,12 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "Mahdi:469122@tcp(127.0.0.1:3306)/mysql?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 	DB = db
+	DB.AutoMigrate(&ToDo{})
 	fmt.Println("Connected to MySQL successfully")
 }
